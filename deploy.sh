26e2ca278051aa9aa13237d7bf01887d0a67358c
yu-andy <37215882+yu-andy@users.noreply.github.com>
2019-08-07 20:32:08 +0930

    added patch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -xe
-
+kubectl patch deployment mywebsite-deployment -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"$(date +%s)\"}}}}}"
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
